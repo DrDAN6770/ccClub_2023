3167800b31d3b92f008bf42e3859921e0e946e43
Dr.Dan <danny6770@gmail.com>
2023-07-03 10:28:58 +0800

    changing function list file

--- ccClub專案/Function_list.py
+++ ccClub專案/Function_list.py
@@ -2,6 +2,7 @@
 from geopy.geocoders import GoogleV3
 from geopy import distance
 import dotenv
+import requests
 
 def get_latitude_longitude(address):
     # load .env
@@ -20,3 +21,10 @@ def get_latitude_longitude(address):
         return None
 def distance_m(location1, location2):
     return distance.distance(location1, location2).meters if location1 and location2 else None
+
+# 文字搜尋編碼化, 返回google map規劃路線網址(模式為:最佳)
+def RoutePlanning(p1, p2):
+    encoded_p1 = requests.utils.quote(p1)
+    encoded_p2 = requests.utils.quote(p2)
+    url = f'https://www.google.com/maps/dir/{encoded_p1}/{encoded_p2}'
+    return url
--- ccClub專案/RoutePlanning.py
+++
@@ -1,7 +0,0 @@
-import requests
-# 文字搜尋編碼化, 返回google map規劃路線網址(模式為:最佳)
-def RP(p1, p2):
-    encoded_p1 = requests.utils.quote(p1)
-    encoded_p2 = requests.utils.quote(p2)
-    url = f'https://www.google.com/maps/dir/{encoded_p1}/{encoded_p2}'
-    return url
